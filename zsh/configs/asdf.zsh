3d625e124180d8f9e59cf4983dacca822272082d
Phil Sturgeon <me@philsturgeon.uk>
2018-06-11 18:01:49 -0400

    Just the once is fine

--- zsh/configs/asdf.zsh
+++ zsh/configs/asdf.zsh
@@ -1,3 +1,10 @@
 source /usr/local/opt/asdf/asdf.sh
 source /usr/local/etc/bash_completion.d
-bash /usr/local/opt/asdf/plugins/nodejs/bin/import-release-team-keyring
+
+
+export NODE_TRUST_FILE=/usr/local/opt/asdf/plugins/nodejs/.trusted
+if [[ ! -f $NODE_TRUST_FILE ]];
+then
+  bash /usr/local/opt/asdf/plugins/nodejs/bin/import-release-team-keyring
+  touch $NODE_TRUST_FILE
+fi
