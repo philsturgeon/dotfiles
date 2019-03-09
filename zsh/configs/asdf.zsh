b296c80700b4a4b8e512034b7e5a70b8a27cb3f8
Phil Sturgeon <me@philsturgeon.uk>
2019-03-09 22:23:35 +0100

    Check two locations for asdf

--- zsh/configs/asdf.zsh
+++ zsh/configs/asdf.zsh
@@ -1,10 +1,19 @@
-source /usr/local/opt/asdf/asdf.sh
-source /usr/local/etc/bash_completion.d
 
 
-export NODE_TRUST_FILE=/usr/local/opt/asdf/plugins/nodejs/.trusted
+export ASDF_NODE_DIR=/usr/local/opt/asdf
+
+# Try the brew location, if not, lets try the home dir
+if [[ ! -d $ASDF_NODE_DIR ]];
+then
+  export ASDF_NODE_DIR=$HOME/.asdf
+fi
+
+source $ASDF_NODE_DIR/asdf.sh
+source $ASDF_NODE_DIR/completions/asdf.bash
+
+export NODE_TRUST_FILE=$ASDF_NODE_DIR/plugins/nodejs/.trusted
 if [[ ! -f $NODE_TRUST_FILE ]];
 then
-  bash /usr/local/opt/asdf/plugins/nodejs/bin/import-release-team-keyring
+  bash $ASDF_NODE_DIR/plugins/nodejs/bin/import-release-team-keyring
   touch $NODE_TRUST_FILE
 fi
