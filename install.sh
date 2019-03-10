b83f51ca19f226deae8fbb5d360a10718865bcd2
Phil Sturgeon <me@philsturgeon.uk>
2019-03-10 14:38:41 +0100

    leave myself a message for next time

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && \
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && \
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
+
+echo "Hopefully that installed brew and ohmyzsh, which which means we can install everything else now!" && \
+sleep 10 && \
 brew tap thoughtbot/formulae && \
 brew install rcm && \
 env RCRC=$HOME/dotfiles/rcrc rcup
-
